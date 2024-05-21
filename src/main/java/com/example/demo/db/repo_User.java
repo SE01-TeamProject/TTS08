148ff47d598f421321df5a8395eafeb9532914d7
ArcRoot <arcroot@cau.ac.kr>
2024-05-21 20:31:02 +0900

    update: repo_User argument type change

--- src/main/java/com/example/demo/db/repo_User.java
+++ src/main/java/com/example/demo/db/repo_User.java
@@ -2,6 +2,6 @@ package com.example.demo.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface repo_User extends JpaRepository<table_User, Long>{
+public interface repo_User extends JpaRepository<table_User, String>{
 
 }
