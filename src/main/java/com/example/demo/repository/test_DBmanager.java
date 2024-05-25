87604d291b5a8cca177efd1636943b0f72c49141
ArcRoot <arcroot@cau.ac.kr>
2024-05-26 03:26:05 +0900

    add: test db manager
    test용 db manager 추가, table별 tuple add 기능 테스트

--- src/main/java/com/example/demo/repository/test_DBmanager.java
+++ src/main/java/com/example/demo/repository/test_DBmanager.java
@@ -1,10 +1,8 @@
 package com.example.demo.repository;
 
 import com.example.demo.db.*;
-import com.example.demo.domain.*;
-import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDateTime;
 
-@Slf4j
 public class test_DBmanager {
     private repo_Issue repoIssue;
     private repo_Comment repoComment;
@@ -12,10 +10,66 @@ public class test_DBmanager {
     private repo_Project repoProject;
     private repo_Log repoLog;
 
-    public void addPL(String userID, String username, String password, int lvl) {
-        PL pl = new PL(userID, username, password, lvl);
+    public void addPL(String userID, String username, String password) {
         table_User user = new table_User();
-        user.setId();
-        repoUser.save(pl);
+        user.setId(userID);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setLevel(1);
+        repoUser.save(user);
+    }
+    public void addDev(String userID, String username, String password) {
+        table_User user = new table_User();
+        user.setId(userID);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setLevel(2);
+        repoUser.save(user);
+    }
+    public void addTester(String userID, String username, String password) {
+        table_User user = new table_User();
+        user.setId(userID);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setLevel(3);
+        repoUser.save(user);
+    }
+
+    public void addProject(Long PID, String title) {
+        table_Project project = new table_Project();
+        project.setPid(PID);
+        project.setTitle(title);
+        repoProject.save(project);
+    }
+    public void addIssue(Long ID, Long PID, String title, String description, String id_reporter, String id_assignee, int priority, int type) {
+        table_Issue issue = new table_Issue();
+        issue.setId(ID);
+        issue.setPriority(priority);
+        issue.setType(type);
+        issue.setTitle(title);
+        issue.setDescription(description);
+        issue.setPid(repoProject.getReferenceById(PID));
+        issue.setReporter(repoUser.getReferenceById(id_reporter));
+        issue.setAssignee(repoUser.getReferenceById(id_assignee));
+        issue.setStatus(0);
+        repoIssue.save(issue);
+    }
+	public void addLog(Long ID, Long PID, String message) {
+        table_Log log = new table_Log();
+        LocalDateTime t=LocalDateTime.now();
+        log.setId(ID);
+        log.setProject_id(repoProject.getReferenceById(PID));
+        log.setLog(message);
+        log.setDate(t);
+        repoLog.save(log);
+    }
+    public void addComment(Long CID, Long Issue_ID, String message) {
+        table_Comment comment = new table_Comment();
+        LocalDateTime t=LocalDateTime.now();
+        comment.setCid(CID);
+        comment.setIssue_id(repoIssue.getReferenceById(Issue_ID));
+        comment.setComment(message);
+        comment.setDate(t);
+        repoComment.save(comment);
     }
 }
