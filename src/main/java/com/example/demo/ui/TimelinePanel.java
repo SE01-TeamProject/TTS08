fc67860429ab37c0f5099cb4df8b9b8d3d8d6ffc
Seonu-32020 <71544916+Seonu-32020@users.noreply.github.com>
2024-05-26 15:33:00 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/ui/TimelinePanel.java
@@ -0,0 +1,31 @@
+package com.example.demo.ui;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+
+public class TimelinePanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public TimelinePanel() {
+		setBorder(new LineBorder(new Color(0, 128, 255), 2, true));
+		setLayout(new BorderLayout(0, 0));
+		
+		JButton btnNewButton = new JButton("Timeline Panel");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("TimelinePanel");
+			}
+		});
+		add(btnNewButton, BorderLayout.CENTER);
+
+	}
+
+}
