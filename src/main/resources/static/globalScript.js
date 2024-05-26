1a2895645d6d28aae21004705a483f45685e1b97
slhun22 <105125575+slhun22@users.noreply.github.com>
2024-05-26 15:14:44 +0900

    add: refactored html, css, js

---
+++ src/main/resources/static/globalScript.js
@@ -0,0 +1,26 @@
+            function gotoHomePage() {
+                location.href = "http://localhost:8080/home.html";
+            }
+            function gotoAdminPage() {
+                //이거 admin인지 권한 확인하는것 필요함
+                location.href = "http://localhost:8080/admin.html";
+            }
+            function gotoIssuePage() {
+                location.href = "http://localhost:8080/issue.html";
+            }
+
+             function hideElements() {
+            const hideableElements = document.querySelectorAll('.hideable');
+            hideableElements.forEach(function(element) {
+                element.style.display = 'none';
+            });
+        }
+
+        function showElements() {
+            const hideableElements = document.querySelectorAll('.hideable');
+            hideableElements.forEach(function(element) {
+                element.style.display = 'block';
+            });
+            document.getElementById('title-input').value = '';
+            document.getElementById('description-input').value = '내용을 입력하세요.';
+        }
