01b10fa5d929ce560c9c4f06f7fb63801fd7e461
Seonu-32020 <71544916+Seonu-32020@users.noreply.github.com>
2024-05-26 23:01:41 +0900

    Add files via upload

--- src/main/java/com/example/demo/Tts08Application.java
+++ src/main/java/com/example/demo/Tts08Application.java
@@ -6,6 +6,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberRepository;
+import com.example.demo.ui.SwingController;
 
 @SpringBootApplication
 public class Tts08Application {
@@ -13,13 +14,26 @@ public class Tts08Application {
 	public static void main(String[] args) {
 		//SpringApplication.run(Tts08Application.class, args);
 
+		System.setProperty("java.awt.headless", "false");
+		
+		
 		// 테스트옹 코드
+		
 		ConfigurableApplicationContext context = SpringApplication.run(Tts08Application.class, args);
 		MemberRepository memberRepository = context.getBean(MemberRepository.class);
 		
 		memberRepository.save(new Member("admin", "Administator", "admin", 0));
 		Member member = memberRepository.findByName("admin");
 		System.out.println("name: " + member.getName() + ", fullname: " + member.getFullName() + ", level: " + member.getLevel());
+		
+		
+		try {
+			SwingController sc = new SwingController();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
