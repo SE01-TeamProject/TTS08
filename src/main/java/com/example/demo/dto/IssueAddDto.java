e3d0a406a388cd6cc5e007725d9b4c2ce666e42d
Richardyun01 <richardyun01@gmail.com>
2024-05-26 22:25:56 +0900

    add issue added

---
+++ src/main/java/com/example/demo/dto/IssueAddDto.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IssueAddDto {
+
+	private String title;
+    private String description;
+//    private String reporter;
+//    private String assignee;
+//    private String priority;
+//    private String status;
+//    private String type;
+
+}
